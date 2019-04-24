6ebba6acea0c1830e2b6a523a1616c66e32132c0
Aruna <aruna_n76@yahoo.com>
2019-04-24 14:17:40 -0400

    Completed Desktop and Mobile Header Styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -16,13 +16,20 @@ gulp.task('watch', function () {
      });
    
    watch('./app/assets/styles/**/*.css', function () {
-        gulp.start('cssInject');  // gulp.start('styles');
-        
+        gulp.start('cssInject');  // gulp.start('styles');        
      });
+
+   watch('./app/assets/scripts/**/*.js', function(){
+       gulp.start('scriptsRefresh');
+   });
+
  }); 
 //injecting css changes to browsersync without reloading the page  //styles task is a dependency of inject task
  gulp.task('cssInject', ['styles'], function(){
     return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
+});
 
+gulp.task('scriptsRefresh', ['scripts'], function(){
+    browserSync.reload();
 });
